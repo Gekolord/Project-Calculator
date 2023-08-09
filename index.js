e94bfdb19244e8de91ab7dc2b3178d9fd2fe4396
Gekolord <80892021+Gekolord@users.noreply.github.com>
2023-08-09 12:58:09 +0300

    add operate function

---
+++ index.js
@@ -0,0 +1,54 @@
+const add = function(n) {
+    let sum = 0
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return sum;
+  };
+  
+  const subtract = function() {
+    let sub = arguments[0]
+    for (let i = 1; i < arguments.length; i++) {
+      sub -= arguments[i];
+    }
+    return sub;
+  };
+  
+  
+  const multiply = function() {
+    let mul = 1
+    for (let i = 0; i < arguments.length; i++) {
+      mul *= arguments[i];
+    }
+    return mul;
+  };
+  
+
+function divide(numbers) {
+    let dividor = arguments[0]
+    for (let i = 1; i < arguments.length; i++) {
+        dividor /= arguments[i]
+    }
+    return dividor
+}
+
+let firstNumber = 0
+let operator = ""
+let secondNumber = 0
+
+function operate(op, n1, n2) {
+    switch (op) {
+        case "+":
+            add(n1,n2);
+            break;
+        case "-":
+            subtract(n1,n2);
+            break;
+        case "*":
+            multiply(n1,n2);
+            break;
+        case "/":
+            divide(n1,n2);
+            break;
+    }
+}
